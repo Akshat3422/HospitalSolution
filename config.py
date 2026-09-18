fef3ba433285cc4b9103aa4cef6535067f71b5f4
Akshat3422 <akshat4899@gmail.com>
2026-09-19 02:31:19 +0530

    initial_setup_completed

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ import os
 load_dotenv()  # Load environment variables from .env file
 
 
+POSTGRES_PASSWORD=os.getenv("POSTGRES_PASSWORD")  # Get the PostgreSQL password from environment variables
 DB_STRING=os.getenv("DATABASE_URL")  # Get the database URL from environment variables
